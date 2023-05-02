72f99b1bbddc1c2bd4be2a09c2483a64c76dd154
doni <daniilmirlanuulu@gmail.com>
2023-05-02 18:05:42 +0600

    fix settings.py

--- ArtColibri_Backend/ArtColibri_Backend/settings.py
+++ ArtColibri_Backend/ArtColibri_Backend/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = f'{BASE_DIR}/{STATIC_URL}'
+STATIC_ROOT = f'{BASE_DIR}/{STATIC_URL}'
 
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [BASE_DIR / 'static', ]
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
