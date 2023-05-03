861a4e8993d0ea89a54d153b0759b6b7caeaeee6
doni <daniilmirlanuulu@gmail.com>
2023-05-04 01:37:40 +0600

    add file build_script.sh

--- ArtColibri_Backend/ArtColibri_Backend/urls.py
+++ ArtColibri_Backend/ArtColibri_Backend/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', IndexView),
                   path('api/v1/', include('ApiRest.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
